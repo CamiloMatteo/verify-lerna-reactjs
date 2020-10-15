d5847b5a4157cdf9c4facbd11b6c3d831869d51a
Camilo Matteo <matteo.camilo.k@gmail.com>
2020-10-15 14:31:26 -0300

    Redirect to not found added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from "react-router-dom"
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom"
 import Layout from "./Layout";
 import Dashboard from "../pages/Dashboard"
+import NotFound from "../pages/NotFound"
 
 const App = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
         <Route exact path="/" component={Dashboard} />
+        <Route path="/404" component={NotFound} />
+        <Redirect from="*" to="/404" />
       </Switch>
     </Layout>
   </BrowserRouter>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "../assets/images/404.svg";
+
+const NotFound = () => (
+  <div class="content has-text-centered">
+    <figure>
+      <img src={Image} alt="Error" width="520" />
+    </figure>
+  </div>
+);
+
+
+export default NotFound;
